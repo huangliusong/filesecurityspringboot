0dc0be99bd2f188c071c7dfb1d7034d0c48e3bda
huangliusong1994 <huangliusong1994@gmail.com>
2019-07-20 11:59:40 +0800

    properties controller test

--- filesecurity-spring-boot-samples/filesecurity-spring-boot-sample-annotation/src/main/java/top/huangliusong1994/spring/SampleAnnotationApplication.java
+++ filesecurity-spring-boot-samples/filesecurity-spring-boot-sample-annotation/src/main/java/top/huangliusong1994/spring/SampleAnnotationApplication.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2015-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2015-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package top.huangliusong1994.spring;
 
@@ -20,11 +20,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+/**
+ * @author huangliusong
+ * @since 2019/7/20
+ */
 @SpringBootApplication
-public class SampleAnnotationApplication{
+public class SampleAnnotationApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(SampleAnnotationApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SampleAnnotationApplication.class, args);
+    }
 
 }
