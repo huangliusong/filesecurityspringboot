67ac2714ff5c9829c8f332b68e392aa0acc505aa
huangliusong1994 <huangliusong1994@gmail.com>
2019-07-20 16:38:12 +0800

    解决本地打包问题

--- build_project_by_hls.sh
+++ build_project_by_hls.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
-echo "#### start build the project #####"
-echo ">>>>  filesecurity-spring-boot-starter   <<<<"
+echo "============================================="
+echo " start build the project "
+echo ">>>> build  filesecurity-spring-boot-starter   <<<<"
+echo "Enter Directory filesecurity"
+cd filesecurity
+ls
+pwd
+echo ">>>> building filesecurity   <<<<"
+mvn clean install
+cd ..
+pwd
+cd filesecurity-spring-boot-autoconfigure
+ls
+pwd
+echo ">>>> building filesecurity-spring-boot-autoconfigure    <<<<"
+mvn clean install
