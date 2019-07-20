340ceeee592d7640145f666689fa5a21df82f8a2
huangliusong1994 <huangliusong1994@gmail.com>
2019-07-20 10:50:02 +0800

    auto

---
+++ filesecurity-spring-boot-autoconfigure/src/main/java/top/huangliusong2019/spring/autoconfigure/FileSecurityAutoConfiguration.java
@@ -0,0 +1,15 @@
+package top.huangliusong2019.spring.autoconfigure;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author huangliusong
+ * @since 2019/7/5
+ * {@link FileSecurityProperties}
+ */
+@Configuration
+@EnableConfigurationProperties(FileSecurityProperties.class)
+public class FileSecurityAutoConfiguration {
+}
---
+++ filesecurity-spring-boot-autoconfigure/src/main/java/top/huangliusong2019/spring/autoconfigure/FileSecurityProperties.java
@@ -0,0 +1,47 @@
+package top.huangliusong2019.spring.autoconfigure;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Configuration properties for  FileSecurity.
+ *
+ * @author huangliusong
+ * @since 2019/7/5
+ * {@link ConfigurationProperties}
+ * {@link EnableAutoConfiguration Auto-configuration}
+ */
+@PropertySource("classpath:application.yml")
+@Configuration
+@ConfigurationProperties(prefix = FileSecurityProperties.FILE_SECURITY_PREFIX)
+public class FileSecurityProperties {
+    public static final String FILE_SECURITY_PREFIX = "file";
+
+    /**
+     * WhiteList
+     */
+    private String uploadPrefixWhiteList;
+
+    /**
+     * BlackList
+     */
+    private String uploadPrefixBlackList;
+
+    public String getUploadPrefixWhiteList() {
+        return uploadPrefixWhiteList;
+    }
+
+    public void setUploadPrefixWhiteList(String uploadPrefixWhiteList) {
+        this.uploadPrefixWhiteList = uploadPrefixWhiteList;
+    }
+
+    public String getUploadPrefixBlackList() {
+        return uploadPrefixBlackList;
+    }
+
+    public void setUploadPrefixBlackList(String uploadPrefixBlackList) {
+        this.uploadPrefixBlackList = uploadPrefixBlackList;
+    }
+}
