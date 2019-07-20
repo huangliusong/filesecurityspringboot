5d2a2dac7658db74f1c676a5caf21c36ffd6ff6f
huangliusong1994 <huangliusong1994@gmail.com>
2019-07-20 12:02:02 +0800

    filesecurity-spring-boot-sample-properties

---
+++ filesecurity-spring-boot-samples/filesecurity-spring-boot-sample-annotation/src/main/java/top/huangliusong1994/spring/PropEntity.java
@@ -0,0 +1,36 @@
+package top.huangliusong1994.spring;
+
+/**
+ * VO
+ *
+ * @author huangliusong
+ * @since 2019/7/20
+ */
+public class PropEntity {
+    private String uploadPrefixWhiteList;
+    private String uploadPrefixBlackList;
+
+    public String getUploadPrefixWhiteList() {
+        return uploadPrefixWhiteList;
+    }
+
+    public void setUploadPrefixWhiteList(String uploadPrefixWhiteList) {
+        this.uploadPrefixWhiteList = uploadPrefixWhiteList;
+    }
+
+    public String getUploadPrefixBlackList() {
+        return uploadPrefixBlackList;
+    }
+
+    public void setUploadPrefixBlackList(String uploadPrefixBlackList) {
+        this.uploadPrefixBlackList = uploadPrefixBlackList;
+    }
+
+    @Override
+    public String toString() {
+        return "PropEntity{" +
+                "uploadPrefixWhiteList='" + uploadPrefixWhiteList + '\'' +
+                ", uploadPrefixBlackList='" + uploadPrefixBlackList + '\'' +
+                '}';
+    }
+}
