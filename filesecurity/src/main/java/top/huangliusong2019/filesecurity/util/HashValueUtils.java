84eafa4ec4c7294eb4dfda9d5c1e8a2b44b7fbc6
huangliusong1994 <huangliusong1994@gmail.com>
2019-07-29 15:47:04 +0800

    commit HashUtils........

---
+++ filesecurity/src/main/java/top/huangliusong2019/filesecurity/util/HashValueUtils.java
@@ -0,0 +1,60 @@
+package top.huangliusong2019.filesecurity.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+
+/**
+ * Hash Value Utils
+ *
+ * @author huangliusong
+ * @since 2019/7/29
+ * {@link MessageDigest}
+ */
+public class HashValueUtils {
+
+    /**
+     * Create MessageDigest by digestType
+     *
+     * @param digestType Hash Type
+     * @return MessageDigest
+     */
+    public static MessageDigest getDigestType(String digestType) {
+        MessageDigest messageDigest = null;
+        switch (digestType) {
+            case "MD5":
+                messageDigest = DigestUtils.getMd5Digest();
+                return messageDigest;
+            case "SHA-1":
+                messageDigest = DigestUtils.getMd5Digest();
+                return messageDigest;
+            case "SHA-256":
+                messageDigest = DigestUtils.getSha256Digest();
+                return messageDigest;
+            case "SHA-384":
+                messageDigest = DigestUtils.getSha384Digest();
+                return messageDigest;
+            case "SHA-512":
+                messageDigest = DigestUtils.getSha512Digest();
+                return messageDigest;
+            default:
+                messageDigest = DigestUtils.getMd5Digest();
+                return messageDigest;
+        }
+    }
+
+    /**
+     * Get Hash Code by file
+     *
+     * @param fileBytes  byte of file
+     * @param digestType digest type
+     * @return HashCode of file
+     */
+    public static String getHashCode(byte[] fileBytes, String digestType) {
+        MessageDigest messageDigest = getDigestType(digestType);
+        messageDigest.update(fileBytes);
+        byte[] digest = messageDigest.digest();
+        return DatatypeConverter.printHexBinary(digest).toUpperCase();
+    }
+}
